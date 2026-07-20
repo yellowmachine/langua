5759c1c7e05f13fe22164fea6de69a8938268ebd
miguel <miguelangel.alarcos@proton.me>
2026-07-20 08:23:22 +0200

    first commit ;)

---
+++ src/lib/server/db/index.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
+import { DATABASE_URL } from '$app/env/private';
+
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+const client = postgres(DATABASE_URL);
+
+export const db = drizzle(client, { schema });
